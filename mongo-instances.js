4fae17c055e501622a95f10c4d97a8f2f377b5e0
David Burles <dburles@gmail.com>
2014-10-18 08:28:06 +1100

    add users collection (if defined)
    thanks @gwendall

--- mongo-instances.js
+++ mongo-instances.js
@@ -41,3 +41,11 @@ Mongo.Collection.get = function(name, options) {
 Mongo.Collection.getAll = function() {
   return instances;
 };
+
+if (Meteor.users) {
+  instances.push({
+    name: 'users',
+    instance: Meteor.users,
+    options: undefined
+  });
+}
