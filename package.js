4abba75b4884cda55b008bd8f8811baa4dcb1604
David Burles <dburles@gmail.com>
2014-12-13 14:07:13 +1100

    version bump

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'dburles:mongo-collection-instances',
   summary: "Access your Mongo instances",
-  version: "0.2.2",
+  version: "0.2.3",
   git: "https://github.com/dburles/mongo-collection-instances.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.3.1');
+  api.versionsFrom('1.0');
   api.use('mongo');
   api.addFiles('mongo-instances.js');
 });
