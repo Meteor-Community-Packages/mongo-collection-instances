39d3191c673637b93b615f8a50aafe33333e77e3
David Burles <dburles@gmail.com>
2014-12-13 14:06:37 +1100

    added another test

--- mongo-instances-tests.js
+++ mongo-instances-tests.js
@@ -1,6 +1,13 @@
-Tinytest.add('basic - works', function (test) {
+Tinytest.add('basic - works Mongo.Collection', function (test) {
   Test = new Mongo.Collection('test' + test.id);
   Test.insert({ test: true });
   var find = Mongo.Collection.get('test' + test.id).find({ test: true });
+  test.equal(find.count(), 1);
+});
+
+Tinytest.add('basic - works Meteor.Collection', function (test) {
+  Test = new Meteor.Collection('test' + test.id);
+  Test.insert({ test: true });
+  var find = Meteor.Collection.get('test' + test.id).find({ test: true });
   test.equal(find.count(), 1);
 });
