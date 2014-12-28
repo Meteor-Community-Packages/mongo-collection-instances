e60e61324533cbb2cd930366290b1517f2546aa4
mikowals <mikowals@gmail.com>
2014-12-28 11:27:08 +1000

    use 'underscore' package for _.extend

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use('mongo');
+  api.use(['mongo', 'underscore']);
   api.addFiles('mongo-instances.js');
 });
 
