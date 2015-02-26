5af22e989d37df6d68c82ba7cdd2901e6589624b
David Burles <dburles@gmail.com>
2015-02-27 10:07:56 +1100

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dburles:mongo-collection-instances',
   summary: "Access your Mongo instances",
-  version: "0.3.2",
+  version: "0.3.3",
   git: "https://github.com/dburles/mongo-collection-instances.git"
 });
 
