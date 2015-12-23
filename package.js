6f27576b30ffd9eb434a1868ef2fa7dd0f7c21e5
pavchip <pavchip@gmail.com>
2015-12-23 14:58:16 +0300

    collection-extensions version bump

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.use([
     'mongo',
     'underscore',
-    'lai:collection-extensions@0.1.4']);
+    'lai:collection-extensions@0.2.1_1']);
   api.addFiles('mongo-instances.js');
 });
 
