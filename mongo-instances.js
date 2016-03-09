640227cff06ab303ea595d6878627aa081bc455e
Ivan S Glazunov <ivansglazunov@gmail.com>
2016-03-10 02:22:42 +0300

    #29

--- mongo-instances.js
+++ mongo-instances.js
@@ -1,21 +1,38 @@
-var instances = [];
+var namedInstances = {};
+var nullInstances = [];
 
 CollectionExtensions.addExtension(function (name, options) {
-  instances.push({
-    name: name,
-    instance: this,
-    options: options
-  });
+  if (name) {
+    if (!(name in namedInstances)) namedInstances[name] = [];
+    namedInstances[name].push({
+      name: name,
+      instance: this,
+      options: options
+    });
+  } else {
+    nullInstances.push({
+      instance: this,
+      options: options
+    });
+  }
 });
 
 Mongo.Collection.get = function(name, options) {
   options = options || {};
-  var collection = _.find(instances, function(instance) {
-    if (options.connection)
-      return instance.name === name &&
-        instance.options && instance.options.connection._lastSessionId === options.connection._lastSessionId;
-    return instance.name === name;
-  });
+  if (name && name in namedInstances) {
+    var collection = _.find(namedInstances[name], function(instance) {
+      if (options.connection)
+        return instance.name === name &&
+          instance.options && instance.options.connection._lastSessionId === options.connection._lastSessionId;
+      return instance.name === name;
+    });
+  } else {
+    var collection = _.find(nullInstances, function(instance) {
+      if (options.connection)
+        return instance.options && instance.options.connection._lastSessionId === options.connection._lastSessionId;
+      return true;
+    });
+  }
 
   return collection && collection.instance;
 };
