aa7ee0478cf6749eeb515a90cad953d37fa82f08
Ivan S Glazunov <ivansglazunov@gmail.com>
2016-03-16 06:03:25 +0300

    Add _ref.

--- mongo-instances.js
+++ mongo-instances.js
@@ -3,6 +3,7 @@ var nullInstances = [];
 
 CollectionExtensions.addExtension(function (name, options) {
   if (options && typeof(options.ref) == 'string') var name = options.ref;
+  this._ref = name;
   if (name) {
     if (!(name in namedInstances)) namedInstances[name] = [];
     namedInstances[name].push({
