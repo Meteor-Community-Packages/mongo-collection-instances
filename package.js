b753ab5e1fbebd69e8a0ec78adf32718318a5aed
Jan Dvorak <storytellercz@protonmail.com>
2022-02-11 23:17:38 +0100

    Update lai:collection-extensions to v0.3
    Updates lai:collection-extensions to the latest version for compatibility with Meteor 2.3+

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dburles:mongo-collection-instances',
   summary: 'Access your Mongo instances',
-  version: '0.3.5',
+  version: '0.3.6',
   git: 'https://github.com/dburles/mongo-collection-instances.git'
 });
 
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.use([
     'mongo',
     'underscore',
-    'lai:collection-extensions@0.2.1_1']);
+    'lai:collection-extensions@0.3.0']);
   api.addFiles('mongo-instances.js');
 });
 
