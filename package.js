78568195210c633d834546159145c0a0f8d050fb
jankapunkt <jkuester@uni-bremen.de>
2024-01-05 20:05:12 +0100

    tests: upgraded test project and code to Meteor 3.0

--- package.js
+++ package.js
@@ -2,26 +2,26 @@
 Package.describe({
   name: 'dburles:mongo-collection-instances',
   summary: 'Access your Mongo instances',
-  version: '1.0.0',
+  version: '1.0.0-beta300.0',
   git: 'https://github.com/dburles/mongo-collection-instances.git'
 })
 
 Package.onUse(function (api) {
   api.versionsFrom(['2.3', '3.0-beta.0'])
   api.use([
-    'mongo',
-    'ecmascript',
-    'lai:collection-extensions@1.0.0'])
+    'ecmascript@0.16.8-beta300.0',
+    'mongo@2.0.0-beta300.0',
+    'lai:collection-extensions@1.0.0-beta300.0'])
   api.addFiles('mongo-instances.js')
 })
 
 Package.onTest(function (api) {
   api.use([
-    'ecmascript',
-    'meteortesting:mocha@2.0.0',
+    'ecmascript@0.16.8-beta300.0',
+    'meteortesting:mocha@3.1.0-beta300.0',
     'accounts-base',
     'mongo@2.0.0-beta300.0',
-    'dburles:mongo-collection-instances@1.0.0'
+    'dburles:mongo-collection-instances@1.0.0-beta300.0'
   ])
   api.addFiles('mongo-instances.tests.js')
 })
