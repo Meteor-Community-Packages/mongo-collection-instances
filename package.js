9dfa67d6e7727ab308484a4dccf3f6226a984582
jankapunkt <jkuester@uni-bremen.de>
2024-04-15 14:31:01 +0200

    fix(build): no explicit versions for core package dependencies

--- package.js
+++ package.js
@@ -2,26 +2,26 @@
 Package.describe({
   name: 'dburles:mongo-collection-instances',
   summary: 'Access your Mongo instances',
-  version: '1.0.0-beta300.1',
+  version: '1.0.0-beta300.2',
   git: 'https://github.com/dburles/mongo-collection-instances.git'
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom(['2.8.0', '3.0-beta.0'])
+  api.versionsFrom(['2.3', '3.0-beta.0'])
   api.use([
-    'ecmascript@0.16.8 || 0.16.8-beta300.0',
-    'mongo@1.16.8 || 2.0.0-beta300.0',
-    'lai:collection-extensions@1.0.0-beta300.1'])
+    'ecmascript',
+    'mongo',
+    'lai:collection-extensions@1.0.0-beta300.2'])
   api.addFiles('mongo-instances.js')
 })
 
 Package.onTest(function (api) {
   api.use([
-    'ecmascript@0.16.8 || 0.16.8-beta300.0',
-    'meteortesting:mocha@2.0.0 || 3.1.0-beta300.0',
-    'accounts-base@2.0.0 || 3.0.0-beta300.0',
-    'mongo@1.16.8 || 2.0.0-beta300.0',
-    'dburles:mongo-collection-instances@1.0.0-beta300.1'
+    'ecmascript',
+    'accounts-base',
+    'mongo',
+    'meteortesting:mocha@3.1.0-beta300.0',
+    'dburles:mongo-collection-instances@1.0.0-beta300.2'
   ])
   api.addFiles('mongo-instances.tests.js')
 })
