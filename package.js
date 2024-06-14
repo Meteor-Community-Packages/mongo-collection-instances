c65670a56cecdf80b543f45126e09de7e5b53396
jankapunkt <jkuester@uni-bremen.de>
2024-06-14 09:36:48 +0200

    published 1.0.0-rc300.1

--- package.js
+++ package.js
@@ -2,16 +2,16 @@
 Package.describe({
   name: 'dburles:mongo-collection-instances',
   summary: 'Access your Mongo instances',
-  version: '1.0.0-beta300.2',
+  version: '1.0.0-rc300.1',
   git: 'https://github.com/dburles/mongo-collection-instances.git'
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom(['2.3', '3.0-beta.0'])
+  api.versionsFrom(['2.3', '3.0-rc.4'])
   api.use([
     'ecmascript',
     'mongo',
-    'lai:collection-extensions@1.0.0-beta300.2'])
+    'lai:collection-extensions@1.0.0-rc300.1'])
   api.addFiles('mongo-instances.js')
 })
 
@@ -20,8 +20,8 @@ Package.onTest(function (api) {
     'ecmascript',
     'accounts-base',
     'mongo',
-    'meteortesting:mocha@3.1.0-beta300.0',
-    'dburles:mongo-collection-instances@1.0.0-beta300.2'
+    'meteortesting:mocha@3.1.0-rc.1',
+    'dburles:mongo-collection-instances@1.0.0-rc300.1'
   ])
   api.addFiles('mongo-instances.tests.js')
 })
