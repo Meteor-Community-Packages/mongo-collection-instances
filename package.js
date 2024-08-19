f2495b2f4c1add917c30d7e718653e1455cf2f24
jankapunkt <jkuester@uni-bremen.de>
2024-08-19 09:15:51 +0200

    build: add 2.3 to api.versionsFrom

--- package.js
+++ package.js
@@ -2,26 +2,28 @@
 Package.describe({
   name: 'dburles:mongo-collection-instances',
   summary: 'Access your Mongo instances',
-  version: '1.0.0-rc300.1',
+  version: '1.0.0',
   git: 'https://github.com/dburles/mongo-collection-instances.git'
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom(['2.3', '3.0-rc.4'])
+  api.versionsFrom(['2.3', '2.8.0', '3.0'])
   api.use([
     'ecmascript',
     'mongo',
-    'lai:collection-extensions@1.0.0-rc300.1'])
+    'lai:collection-extensions@1.0.0'])
   api.addFiles('mongo-instances.js')
 })
 
 Package.onTest(function (api) {
+  api.versionsFrom(['2.3', '2.8.0', '3.0'])
   api.use([
     'ecmascript',
     'accounts-base',
     'mongo',
-    'meteortesting:mocha@3.1.0-rc.1',
-    'dburles:mongo-collection-instances@1.0.0-rc300.1'
+    'typescript',
+    'meteortesting:mocha@3.2.0',
+    'dburles:mongo-collection-instances@1.0.0'
   ])
   api.addFiles('mongo-instances.tests.js')
 })
